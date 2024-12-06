2f5f1ddd10f23c8df7949bb4f6825f5cb0a9a019
oxanagral <oxana.gral@etud.univ-jfc.fr>
2024-12-06 17:54:30 +0100

    Zoom.java

--- src/main/java/TP/Zoom.java
+++ src/main/java/TP/Zoom.java
@@ -1,1 +1,29 @@
+package TP;
 
+import bandeau.Bandeau;
+import java.awt.Font;
+
+public class Zoom extends Effet {
+    private int etapes;
+    private int delai;
+
+    public Zoom(Bandeau bandeau, int etapes, int delai) {
+        super(bandeau);
+        this.etapes = etapes;
+        this.delai = delai;
+    }
+
+    @Override
+    public void jouer() {
+        Font fontOrigine = bandeau.getFont();
+        int tailleOrigine = fontOrigine.getSize();
+
+        for (int i = 0; i < etapes; i++) {
+            bandeau.setFont(new Font(fontOrigine.getName(), fontOrigine.getStyle(), tailleOrigine + i));
+            bandeau.sleep(delai);
+        }
+
+        // Restaurer la taille d'origine
+        bandeau.setFont(fontOrigine);
+    }
+}
