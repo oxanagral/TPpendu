5b993a753ee0cb453091adc6e720e9ae7b4c6303
oxanagral <oxana.gral@etud.univ-jfc.fr>
2024-12-06 17:54:01 +0100

    Teletype.java

--- src/main/java/TP/Teletype.java
+++ src/main/java/TP/Teletype.java
@@ -1,1 +1,24 @@
+package TP;
 
+import bandeau.Bandeau;
+
+public class Teletype extends Effet {
+    private int delai;
+
+    public Teletype(Bandeau bandeau, int delai) {
+        super(bandeau);
+        this.delai = delai;
+    }
+
+    @Override
+    public void jouer() {
+        String message = bandeau.getMessage();
+        StringBuilder affichage = new StringBuilder();
+
+        for (char c : message.toCharArray()) {
+            affichage.append(c);
+            bandeau.setMessage(affichage.toString());
+            bandeau.sleep(delai);
+        }
+    }
+}
