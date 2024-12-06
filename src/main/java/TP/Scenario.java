5f394d7f6f9281988a49b259bdd222547e0f5640
oxanagral <oxana.gral@etud.univ-jfc.fr>
2024-12-06 17:53:29 +0100

    Scenario.java

--- src/main/java/TP/Scenario.java
+++ src/main/java/TP/Scenario.java
@@ -1,1 +1,27 @@
+package TP;
 
+import bandeau.Bandeau;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scenario {
+    private List<Effet> effets;
+    private Bandeau bandeau;
+
+    public Scenario(Bandeau bandeau) {
+        this.bandeau = bandeau;
+        this.effets = new ArrayList<>();
+    }
+
+    public void ajouterEffet(Effet effet, int repetitions) {
+        for (int i = 0; i < repetitions; i++) {
+            effets.add(effet);
+        }
+    }
+
+    public void jouer() {
+        for (Effet effet : effets) {
+            effet.jouer();
+        }
+    }
+}
